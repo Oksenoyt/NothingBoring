452a0738021db57c975ae48e94ff970e8c83f41f
Oksenoyt <a.oksenoyt@gmail.com>
2023-07-02 14:16:50 -0700

    Initial Commit

---
+++ Not_boring/Not_boringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Not_boringApp.swift
+//  Not_boring
+//
+//  Created by Oksenoyt on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Not_boringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
