019d7364e31534474340da9f62dfd3d4cabd6ef8
Oksenoyt <a.oksenoyt@gmail.com>
2023-07-12 13:01:28 -0700

    added windmill with animation

--- Not_boring/RandomActivity.swift
+++ Not_boring/RandomActivity.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RandomActivity: View {
     private let typeActivity = TypeActivity .general
 
+    @State private var rotationAngle: Angle = .degrees(0)
+
     @StateObject private var viewModel = ViewModel()
 
     var body: some View {
@@ -33,6 +35,35 @@ struct RandomActivity: View {
                 }
                 .offset(y: 4)
                 .padding(25)
+                Spacer()
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                HStack {
+                    Spacer()
+                    ZStack {
+                                Image("windmill")
+                                    .resizable()
+                                    .scaledToFit()
+                                Image("windmill2")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .rotationEffect(rotationAngle, anchor: .center)
+                                    .offset(x: -12 ,y: -132)
+                            }
+                            .onAppear {
+                                withAnimation(Animation.linear(duration: 5.0).repeatForever(autoreverses: false)) {
+                                    rotationAngle = .degrees(360)
+                                }
+                            }
+                            .frame(width: 250)
+                            .opacity(0.8)
+                            .offset(y: -13)
+                }
                 Spacer()
                 Spacer()
             }
